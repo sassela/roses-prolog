1c9c04a1173b7b15d45820a83a2d45a4e4750254
= <sassela@live.com>
2015-11-18 07:44:42 +0000

    Begin solution predicate

--- knowledge_base.pl
+++ knowledge_base.pl
@@ -25,6 +25,41 @@ event('anniversary party').
 event('retirement banquet').
 
 
+solve :- 
+	rose(IdaRose),
+	rose(JeremyRose),
+	rose(HughRose),
+	rose(LeroyRose),
+	rose(StellaRose),
+	all_different([IdaRose, JeremyRose, HughRose, LeroyRose, StellaRose]),
+
+	item(IdaItem),
+	item(JeremyItem),
+	item(HughItem),
+	item(LeroyItem),
+	item(StellaItem),
+	all_different([IdaItem, JeremyItem, HughItem, LeroyItem, StellaItem]),
+
+	event(IdaEvent),
+	event(JeremyEvent),
+	event(HughEvent),
+	event(LeroyEvent),
+	event(StellaEvent),
+	all_different([IdaEvent, JeremyEvent, HughEvent, LeroyEvent, StellaEvent]),
+
+	Quadruples = [ [ida, IdaRose, IdaItem, IdaEvent],
+	      	   [jeremy, JeremyRose, JeremyItem, JeremyEvent],
+	           [hugh, HughRose, HughItem, HughEvent],
+	           [leroy, LeroyRose, LeroyItem, LeroyEvent],
+	           [stella, StellaRose, StellaItem, StellaEvent] ],
+
+   	tell(ida, IdaRose, IdaItem, IdaEvent),
+   	tell(jeremy, JeremyRose, JeremyItem, JeremyEvent),
+   	tell(hugh, HughRose, HughItem, HughEvent),
+   	tell(leroy, LeroyRose, LeroyItem, LeroyEvent),
+   	tell(stella, StellaRose, StellaItem, StellaEvent).
+
+
 % Succeeds if all elements of the argument list are bound and different. 
 % Fails if any elements are unbound or equal to some other element.
 
